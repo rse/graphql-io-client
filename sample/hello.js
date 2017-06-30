3b05e73a019713ac9d21abfdad0c73e552b22613
Ralf S. Engelschall <rse@engelschall.com>
2017-06-30 10:52:30 +0200

    reduce to minimum

--- sample/hello.js
+++ sample/hello.js
@@ -2,7 +2,7 @@
 const { Client } = require("graphql-io-client")
 
 ;(async () => {
-    const sv = new Client({ url: "http://127.0.0.1:12345/api", debug: 0 })
+    const sv = new Client({ url: "http://127.0.0.1:12345/api" })
     sv.on("debug", ({ log }) => console.log(log))
     await sv.connect()
     let result = await sv.query("{ hello }")
